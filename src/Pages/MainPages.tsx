8954cb1cc7e7ee65eb86f694e76bdca716434023
Nazarkulovv <ramazanwelll@gmail.com>
2024-05-04 15:27:35 +0600

    basket in header

--- src/Pages/MainPages.tsx
+++ src/Pages/MainPages.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import BasketHeader from '../components/BasketHeader/BasketHeader'
+import Basket from '../components/Basket/Basket'
 
 const MainPages: React.FC = () => {
-	return <div></div>
+	return (
+		<div>
+			<BasketHeader />
+			<Basket/>
+		</div>
+	)
 }
 
 export default MainPages
